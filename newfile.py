930e1f4d7eac4efea56d2d82f0f7cd6ff486b9e0
Aakash <aakashdhiman1234@gmail.com>
2021-04-23 18:48:26 +0530

    Adding new file

---
+++ newfile.py
@@ -0,0 +1,8 @@
+import datetime
+import sys  
+import os    
+print(datetime.datetime.today())    
+print(sys.version)
+print(sys.argv)
+print(os.getcwd())
+os.mkdir("D:\Cloudeq\My Learning\PythonProject\Aakash")
